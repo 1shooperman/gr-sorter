42d8662294b2cc89e231280ac7b6e580eb202536
Brandon Shoop <brandon.shoop@boats.com>
2018-08-27 07:26:39 -0400

    remove unneeded setup.py * inspired by https://caremad.io/posts/2013/07/setup-vs-requirement/

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='Reading List Sorter',
-    version='0.0.1-alpha',
-    packages=['sorter',],
-    license='MIT',
-    long_description=open('README.md').read(),
-)
