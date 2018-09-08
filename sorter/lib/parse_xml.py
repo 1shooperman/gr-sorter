95485804c0840b2672d63e39e4bb0c7a90654cbb
Brandon Shoop <brandon.shoop@boats.com>
2018-09-08 12:26:38 -0400

    fix unique constraint error

--- sorter/lib/parse_xml.py
+++ sorter/lib/parse_xml.py
@@ -14,17 +14,23 @@ def parse(xml_string):
 
 def get_book_data(generator):
     ''' private method used for pulling book data from the provided xml generator '''
+    pub_year = generator.find('book/published').text
+    if pub_year is None:
+        pub_year = generator.find('book/publication_year').text
+
     book = (
         int(generator.find('id').text),
         generator.find('book/isbn').text,
         generator.find('book/isbn13').text,
         generator.find('book/title_without_series').text,
         generator.find('book/image_url').text,
-        generator.find('book/publication_year').text,
+        pub_year,
         int(generator.find('book/ratings_count').text),
         generator.find('book/average_rating').text,
         generator.find('book/authors/author/name').text
     )
+
+    
 
     return book
 
