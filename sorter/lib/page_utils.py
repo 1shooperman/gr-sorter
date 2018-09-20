0b7d750465fc8bbfc0450901fc89c57af2feaedc
Brandon Shoop <brandon.shoop@boats.com>
2018-09-19 20:13:42 -0400

    adding a separate cleaning endpoint

--- sorter/lib/page_utils.py
+++ sorter/lib/page_utils.py
@@ -1,6 +1,8 @@
 ''' page_utils.py '''
 import os
 
+from urlparse import parse_qs, urlsplit
+
 from sorter.lib.data_handler import store_data, dump_data
 from sorter.lib.parse_xml import parse
 from sorter.lib.bootstrap import bootstrap
@@ -19,3 +21,25 @@ def page_loop(xml_data, db_name, new_data=False):
         bootstrap(db_name, LOGGER)
 
     store_data(filtered_data, db_file)
+
+def page_vars(post_data):
+    '''
+    Get page vars from POST data
+    '''
+    _, _, _, query, _ = urlsplit(post_data)
+    args = parse_qs(query)
+
+    new_data = False
+    per_page = None
+    api_key = None
+    user_id = None
+
+    try:
+        new_data = int(args['new'][0]) == 1
+        per_page = int(args['per_page'][0])
+        api_key = args['api_key'][0]
+        user_id = args['user_id'][0]
+    except KeyError:
+        LOGGER.warn(KeyError)
+
+    return (new_data, per_page, api_key, user_id)
